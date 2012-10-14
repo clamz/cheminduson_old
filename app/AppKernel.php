f315286528df73a8f0a83c0853d0ab355f066db5
Guezou Damien <damien.guezou@gmail.com>
2012-10-14 20:04:43 +0200

    Ajout du bundle knpMenuBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         	new FOS\UserBundle\FOSUserBundle(),
             new Clamz\CdsBundle\ClamzCdsBundle(),
+        	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
